b20fc662477fe3896caf8caef09afa98e5437984
user <user@iii-user-PC>
2018-07-22 14:43:32 +0800

    ok

---
+++ joe/src/tw/org/iii/mytest/joe05.java
@@ -0,0 +1,15 @@
+package tw.org.iii.mytest;
+
+public class joe05 {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		System.out.println(c1);
+		char c2 = '資';
+		System.out.println(c2);
+		char c3 = 65; //0-65535
+		System.out.println(c3); //ASCII code
+		System.out.println(0x41); //0開頭圍8進位, 0x開頭圍16進位
+	}
+
+}
